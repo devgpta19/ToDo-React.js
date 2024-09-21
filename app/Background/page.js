0f6473021314062769bef658d455fed7d7a8a9fc
Devansh Gupta <devansh2002gupta@gmail.com>
2024-09-21 17:31:29 +0530

    Commit

--- app/Background/page.js
+++ app/Background/page.js
@@ -4,8 +4,12 @@ function Background() {
     return (
         <>
             <div className=' fixed z-[2] flex justify-center items-center w-full h-screen bg-zinc-800'>
-                <h1 className='text-[13vw] font-semibold text-zinc-900 leading-none tracking-tighter'>todo.</h1>
+                <div className=''>
+                    <h1 className='text-[13vw] text-center font-semibold text-zinc-900 leading-none tracking-tighter'>todo.</h1>
+                    <h2 className='text-center text-zinc-300/40 mb-10' >Try to <span className='text-green-500' >DRAG</span> your <span className='text-green-500' >TASKS</span>.</h2>
+                </div>
             </div>
+
         </>
     )
 }
