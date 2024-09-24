37f5d0fd4b27a2c93866bc19e9362676610d6bfe
Devansh Gupta <devansh2002gupta@gmail.com>
2024-09-25 00:04:04 +0530

    Local Storage
    Use of "localstorage" to save the items locally on browser.

--- app/Foreground/page.js
+++ app/Foreground/page.js
@@ -1,9 +1,22 @@
 "use client"
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { IoIosAddCircle } from "react-icons/io";
 import Link from 'next/link'
 import MoreCards from '@/app/Listcards/page';
 import { motion } from "framer-motion"
+
+
+
+const getLocalItems = () =>{
+    let list = localStorage.getItem('todo-list')
+
+    if(list){
+        return JSON.parse(list);
+    }
+    else{
+        return []
+    }
+}
 
 
 function Foreground() {
@@ -11,7 +24,7 @@ function Foreground() {
     const inputRef = useRef(null);
 
     const [task, setTask] = useState("")
-    const [maintask, setMaintask] = useState([]);
+    const [maintask, setMaintask] = useState(getLocalItems());
 
     const fun = (e) => {
         setTask(e.target.value)
@@ -31,6 +44,15 @@ function Foreground() {
     ))
 
 
+    useEffect(() => {
+      return () => {
+        localStorage.setItem('todo-list',JSON.stringify(maintask))
+      }
+    }, [maintask])
+    
+    console.log(maintask);
+
+
     return (
         <div className='w-full h-screen '>
             <div ref={inputRef} className='fixed p-5 top-0 left-0 z-[2] w-full h-full '>
@@ -38,7 +60,7 @@ function Foreground() {
                     <div className='relative overflow-hidden mt-5 z-[3] flex justify-center'>
                         <form onSubmit={submitHandler}>
                             <div className='rounded-[20px] flex gap-5'>
-                                <input type="text" placeholder='Enter your task' value={task} onChange={fun} className='placeholder:text-sky-900 p-2 bg-slate-400/60 rounded w-52 flex justify-center text-center text-zinc-950'/>
+                                <input type="text" placeholder='Enter your task' value={task} onChange={fun} className='placeholder:text-sky-900 p-2 bg-slate-300 rounded w-52 flex justify-center text-center text-zinc-950'/>
                                 <button className='bg-sky-600 text-slate-100 px-5 py-2 rounded-2xl'>ADD</button>
                             </div>
                         </form>
--- app/Listcards/page.js
+++ app/Listcards/page.js
@@ -5,21 +5,24 @@ import { motion } from "framer-motion"
 
 
 const MoreCards = ({ ind, ite, copy, setmain, reference }) => {
+
     const deleteHandler = (ind) => {
+        console.log(ind);
         let copyArray = [...copy]
         copyArray.splice(ind, 1)
+        console.log(copyArray);
         setmain(copyArray)
     }
 
     return (
         <motion.div drag dragConstraints={reference} key={ind} className='text-zinc-100 bg-zinc-900/50 p-10 text-sm w-60 rounded-[70px] mt-5 '>
-            <h2 className='text-xs font-bold flex justify-center mb-5 '>
+            <h2 className='text-xs font-bold text-green-600 flex justify-center mb-5 '>
                 TASK:{ind + 1}
             </h2>
-            <p>{ite}</p>
+            <p className='font-semibold flex justify-center'>{ite}</p>
             <button onClick={() => {
                 deleteHandler(ind)
-            }} className='p-5 rounded-[50px] bg-green-500/40 w-full mt-10'>
+            }} className='p-5 rounded-[50px] buttom-10 bg-green-500/40 w-full mt-[3vw]'>
                 END Task
             </button>
         </motion.div>
